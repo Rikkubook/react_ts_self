04d424cdc978a108d22b61a6db16c6cb7c1b69a3
Rikku <rikkubook@gmail.com>
2024-01-24 17:46:06 +0800

    ✨ feat(router): 添加router

--- src/pages/index/App.tsx
+++ src/pages/index/App.tsx
@@ -3,20 +3,6 @@ import Header from "@/components/Header";
 // import "./style.css";
 
 function App() {
-  const {
-    register,
-    handleSubmit,
-    watch,
-    formState: { errors },
-  } = useForm({
-    defaultValues: {
-      example: "",
-      exampleRequired: "",
-    },
-  });
-
-  console.log(watch("example")); // you can watch individual input by pass the name of the input
-
   return (
     <>
       <Header />
